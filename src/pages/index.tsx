86552e6d1d834b9bc9e543f3a2f083981012daac
pisa <pisa0401mst@gmail.com>
2022-10-16 23:47:16 +0900

    show sample image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,10 @@
 const IndexPage = () => {
-    return <h1>Hello, Next.js</h1>;
+    return (
+        <div>
+            <h1>Hello, Next.js</h1>
+            <img src="https://idolmaster-official.jp/idolmaster/jp/article/1002/SHINYCOLORS/2022/04/%E7%94%BB%E5%83%8F%E2%91%A0_%E6%9D%9C%E9%87%8E%E5%87%9B%E4%B8%96%E3%83%95%E3%82%A3%E3%82%AE%E3%83%A5%E3%82%A2.jpg?_=1665931560"></img>
+        </div>
+    );
 };
 
 export default IndexPage;
